009037c7492df25b38d3258a40cb22bc3e0e170c
Tyler Simpson <Tyler.Simpson@digicert.com>
2025-03-28 09:43:58 -0600

    adding starter project

---
+++ src/main/java/org/example/springbootdemok8s/SpringBootDemoK8sApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootdemok8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoK8sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootdemok8s/SpringBootDemoK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootdemok8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
